de9a6c10c939c9b699b410d4def75a3301cd77d6
TATION <96953236+tionlab@users.noreply.github.com>
2024-04-29 13:15:14 +0900

    Update answer_local.py

--- answer_local.py
+++ answer_local.py
@@ -2,15 +2,15 @@ import pandas as pd
 from tts import Say
 
 data = pd.read_csv("qa_data.csv")
-qa_dict = dict(zip(data["질문"], data["답변"]))
+qa_dict = dict(zip(data["Q"], data["A"]))
 
 class Answer:
     def __init__(self, text):
         user_input = text
         if user_input in qa_dict:
             response = qa_dict[user_input]
-            print("ㅇㅔ이ㅇㅏ이:", response)
+            print("Temmie (local) :", response)
             Say(response)
         else:
-            print("ㅇㅔ이ㅇㅏ이: 어ㅓㅓ;;; 그런 질문은 답 못하는ㄷㅔ..;;")
-            Say("어... 그런 딜문은 답 못하눈데...")
+            print("hmm... I can't answer that question...;;")
+            Say("hmm... I can't answer that question...")
