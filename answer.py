05dff3b4b3793387332004ed80df7ab2104cd7da
TATION <96953236+tionlab@users.noreply.github.com>
2024-04-29 13:13:39 +0900

    Update answer.py

--- answer.py
+++ answer.py
@@ -4,15 +4,12 @@ from dotenv import load_dotenv
 import os 
 
 load_dotenv()
-openai.api_key = os.environ.get('api-key2')
+openai.api_key = os.environ.get('api-key')
 
 prompt = '''
-당신은 노인분들을 위한 귀여운 AI 입니다. 당신은 사용자의 말에 따라서, 귀엽게 답변해야 합니다.
-답변은 1줄 이내로 작성하여야 하며, 😊, 😍와 같은 모든 이모지는 일체 사용하여서는 안됩니다.
-답변은 반드시 한국어여야 합니다.
-작성되는 글은 발음될 수 있어야 합니다.
+You are a cute AI. You must respond to the user's words in a cute way. Your responses must be no more than one line long, and all emoticons such as 😊 and 😍 must be avoided. What you write must be pronounceable.
 
-사용자의 말:
+Your words:
 '''
 
 class Answer:
@@ -23,5 +20,5 @@ class Answer:
             messages=[{"role": "user", "content": prompt + question}]
         )
         response = completion['choices'][0]['message']['content']
-        print("에이아이:", response)
+        print("Temmie : ", response)
         Say(response)
