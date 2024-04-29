92e550a5b9148183d8cae33498d6e1ecce0a763a
TATION <96953236+tionlab@users.noreply.github.com>
2024-04-29 13:17:03 +0900

    Update stt.py

--- stt.py
+++ stt.py
@@ -8,13 +8,13 @@ class Listen:
         
         try:
             with sr.Microphone() as source:
-                print("듣는 중 이ㅇㅔㅔㅔ요!")
+                print("Listeningggg!")
                 self.recognizer.adjust_for_ambient_noise(source)
                 self.audio = self.recognizer.listen(source, timeout=None, phrase_time_limit=3)
             self.text = self.recognizer.recognize_google(self.audio, language="ko-KR")
-            print("ㄴㅏ : " +  self.text)
+            print("You : " +  self.text)
         except sr.UnknownValueError:
-            print("머ㅓㅝ라ㅏ고요?")
+            print("Pardon?")
         except sr.RequestError as e:
-            print("갑ㅈㅏ기 ㅇㅏㄴ들리는 ㄱㅓㅅ 같아ㅏㅇ...;; {0}".format(e))
+            print("Suddenly I can't hear you... {0}".format(e))
 
